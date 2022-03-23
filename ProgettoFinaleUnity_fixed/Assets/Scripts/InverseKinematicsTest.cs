2b3212adcd16ad1ab2ab5e2b8a5c3eda2c6d4fc6
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-03-23 23:51:17 +0100

    End of Day Commit revised

--- ProgettoFinaleUnity_fixed/Assets/Scripts/InverseKinematicsTest.cs
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/InverseKinematicsTest.cs
@@ -10,14 +10,18 @@ public class InverseKinematicsTest : MonoBehaviour
     public bool GunEquipped = false;
     public Rig Rig;
     public TwoBoneIKConstraint restingGun,shootingGun;
+    public Rig LookAtConstraintRIG;
     public Transform TargetIKShooting;
+    private Transform CameraReference;
     public float target=0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+        CameraReference = GetComponent<PlayerControllerSecondVersion>().CameraReference;
         GetComponent<PlayerControllerSecondVersion>().controls.Player.Shot.performed += ManageShooting;
         GetComponent<PlayerControllerSecondVersion>().controls.Player.EquipWeapon.performed += EquipGun;
+        GetComponent<PlayerControllerSecondVersion>().controls.Player.RotateCamera.performed += CheckInverseKinematics;
+        GetComponent<PlayerControllerSecondVersion>().controls.Player.Movement.performed += CheckInverseKinematics;
 
     }
     void ManageShooting(InputAction.CallbackContext ctx)
@@ -72,6 +76,22 @@ public class InverseKinematicsTest : MonoBehaviour
             Rig.weight = 0f;
         }
         
+    }
+    void CheckInverseKinematics(InputAction.CallbackContext ctx)
+    {
+        float angleRadians = Vector3.Dot(this.transform.forward, CameraReference.forward);
+        angleRadians *= 180f / 3.14f;
+        //angleRadians = Mathf.Abs(angleRadians);
+        Debug.Log(angleRadians);
+        if (angleRadians < -50f)
+        {
+            LookAtConstraintRIG.weight = Mathf.Lerp(LookAtConstraintRIG.weight, 0f, 0.1f);
+        }
+        else
+        {
+            LookAtConstraintRIG.weight = Mathf.Lerp(LookAtConstraintRIG.weight, 0.75f, 0.1f);
+
+        }
     }
     void Shoot()
     {
