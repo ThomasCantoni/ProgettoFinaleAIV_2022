dc7583ff907487e42242c298b3e29fb924db15df
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-03-26 20:30:15 +0100

    Started UI and Started Health system for Ellen

---
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/DamagePlayer.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DamagePlayer : MonoBehaviour
+{
+    private void OnTriggerStay(Collider collision)
+    {
+        EllenHealthScript attempt = new EllenHealthScript();
+        if(collision.gameObject.TryGetComponent(out attempt))
+        {
+           attempt.DamagePlayer(10f * Time.deltaTime);
+        }
+    }
+}
