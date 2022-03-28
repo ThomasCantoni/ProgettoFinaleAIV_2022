13db1ad14a1695244dd4982ec017aedea2204297
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-03-28 22:17:29 +0200

    Started Creating Portals

---
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/PortalScript.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalScript : MonoBehaviour
+{
+    public Transform TeleportDestination;
+   
+
+    public void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("TELEPORTING "+ other.gameObject.name);
+        other.gameObject.GetComponent<CharacterController>().enabled = false;
+        other.gameObject.transform.position = TeleportDestination.position;
+        other.gameObject.GetComponent<CharacterController>().enabled = true;
+
+    }
+}
