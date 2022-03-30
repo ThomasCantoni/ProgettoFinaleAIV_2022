724682c9bf04180ce31d1fa18fd58e9d9ff8549f
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-03-30 11:45:26 +0200

    Created build

---
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/BulletTimeListenerText.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+public class BulletTimeListenerText : MonoBehaviour
+{
+    public PlayerControllerSecondVersion PCSV;
+    private Controls controls;
+    void Start()
+    {
+        controls = PCSV.controls;
+        controls.Player.BulletTimeInput.performed += Toggle;
+        this.gameObject.SetActive(false);
+        
+    }
+
+    void Toggle(InputAction.CallbackContext ctx)
+    {
+        this.gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+}
