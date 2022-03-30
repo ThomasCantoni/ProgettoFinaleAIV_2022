edbe75b8f78c49648df44b559371c457461bb051
Deomoreo <deomoreo@gmail.com>
2022-03-30 18:01:33 +0200

    Add New Animations Enemy

--- ProgettoFinaleUnity_fixed/Assets/Scripts/Chomper.cs
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/Chomper.cs
@@ -2,23 +2,40 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.AI;
+
 
 public class Chomper : MonoBehaviour, IHittable
 {
     public int Health;
     public UnityEvent<Collider> OnHitEvent;
+    public Animator anim;
+    public NavMeshAgent agent;
 
+    float timer = 0f;
     public virtual void OnHit(Collider sender)
     {
         OnHitEvent?.Invoke(sender);
-
         Health--;
-        Debug.Log(Health);
+    }
 
+    void rip()
+    {
+        //Destroy(transform.parent.gameObject);
         if (Health <= 0)
         {
-            //Destroy(transform.parent.gameObject);
-            gameObject.SetActive(false);
+            agent.speed = 0;
+            timer += Time.deltaTime;
+            anim.SetTrigger("rip");
+            if (Health <= 0 && timer >= 1.35f)
+            {
+                gameObject.SetActive(false);
+            }
         }
+        
+    }
+    private void Update()
+    {
+        rip();
     }
 }
