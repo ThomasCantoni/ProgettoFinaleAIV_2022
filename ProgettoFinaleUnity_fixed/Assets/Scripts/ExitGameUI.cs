75ebd3d8cab5276f788b9aa8152c868f1d88ef7d
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-04-04 10:59:38 +0200

    FixedPause, added button to main menu

--- ProgettoFinaleUnity_fixed/Assets/Scripts/ExitGameUI.cs
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/ExitGameUI.cs
@@ -1,11 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class ExitGameUI : MonoBehaviour
 {
    public void CloseGame()
     {
         Application.Quit();
+    }
+    public void BackToMenu()
+    {
+        SceneManager.LoadScene(0, LoadSceneMode.Single);
     }
 }
