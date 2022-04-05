9ce45487819bedfdeceb1a5bd9ccafac626c3118
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-04-05 12:13:01 +0200

    Hp bars made

--- ProgettoFinaleUnity_fixed/Assets/Scripts/Chomper.cs
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/Chomper.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.AI;
+using UnityEngine.UI;
+
 
 
 public class Chomper : MonoBehaviour, IHittable
@@ -11,18 +13,21 @@ public class Chomper : MonoBehaviour, IHittable
     public UnityEvent<Collider> OnHitEvent;
     public Animator anim;
     public NavMeshAgent agent;
+    public Slider HP_Slider;
 
     float timer = 1.35f;
     public virtual void OnHit(Collider sender)
     {
         OnHitEvent?.Invoke(sender);
         Health--;
+        HP_Slider.value = Health;
         if (Health <= 0)
         {
             agent.speed = 0;
             timer += Time.deltaTime;
             anim.SetTrigger("rip");
             StartCoroutine(animDie(timer));
+            HP_Slider.transform.parent.gameObject.SetActive( false);
         }
     }
     public IEnumerator animDie(float timer)
