431e80d8fcdf348fdbaa68b7dc1da34ffea704a7
Deomoreo <deomoreo@gmail.com>
2022-04-05 16:59:09 +0200

    Update CameraOut.cs

--- ProgettoFinaleUnity_fixed/Assets/Scripts/CameraOut.cs
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/CameraOut.cs
@@ -9,7 +9,7 @@ public class CameraOut : MonoBehaviour
     public float targetFOV;
     private void Start()
     {
-        targetFOV = cam.m_Lens.FieldOfView + 20f;
+        targetFOV = cam.m_Lens.FieldOfView + 30f;
         if (cam.m_Lens.FieldOfView >= 90f)
         {
             targetFOV = cam.m_Lens.FieldOfView - 20f;
