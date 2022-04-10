214726832430908b5171baae2a33e41b656f61fb
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-04-10 22:39:13 +0200

    tired 2

--- ProgettoFinaleUnity_fixed/Assets/Scripts/DoorEnemyCounter.cs
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/DoorEnemyCounter.cs
@@ -5,22 +5,27 @@ using UnityEngine.Events;
 
 public class DoorEnemyCounter : MonoBehaviour
 {
-    [SerializeField]
-    public List<Chomper> Enemies;
+    
+    public Transform[] Enemies;
     public UnityAction OnEnemyDeath;
     public int AmountDead =0;
     void Start()
     {
         OnEnemyDeath = IncreaseDead;
-        foreach(Chomper x in Enemies)
+        for(int i=0;i<Enemies.Length;i++)
         {
-            x.OnDeath.AddListener(OnEnemyDeath);
+
+            Enemies[i].GetComponent<Chomper>().OnDeath.AddListener(OnEnemyDeath);
         }
+        
+        
+        
     }
+   
     void IncreaseDead()
     {
         AmountDead++;
-        if(AmountDead >= Enemies.Count)
+        if(AmountDead >= Enemies.Length)
         {
             this.GetComponent<Animator>().SetBool("isDoorOpen",true);
 
