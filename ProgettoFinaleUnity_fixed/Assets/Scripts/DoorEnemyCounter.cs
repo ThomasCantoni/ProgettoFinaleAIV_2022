97bdd8349a76346885aec84b12adf140ca38b81c
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-04-10 21:46:31 +0200

    sono stanco

---
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/DoorEnemyCounter.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorEnemyCounter : MonoBehaviour
+{
+    [SerializeField]
+    public List<Chomper> Enemies;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
