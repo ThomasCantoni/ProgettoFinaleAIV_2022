f62dfa228c89eaa72961a9fe212c0863fdd18dd2
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-04-18 18:52:41 +0200

    placed sound

--- ProgettoFinaleUnity_fixed/Assets/Scripts/PlaySound.cs
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/PlaySound.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class PlaySound : MonoBehaviour
 {
    public  AudioSource AS;
-    public AudioClip Clip;
+    public AudioClip Clip,Clip4,Clip3;
     // Start is called before the first frame update
     void Start()
     {
@@ -17,6 +17,22 @@ public class PlaySound : MonoBehaviour
         if(Clip != null)
         {
             AS.clip = Clip;
+        }
+        AS.Play();
+    }
+    public void PlaySource4()
+    {
+        if (Clip != null)
+        {
+            AS.clip = Clip4;
+        }
+        AS.Play();
+    }
+    public void PlaySource3()
+    {
+        if (Clip != null)
+        {
+            AS.clip = Clip3;
         }
         AS.Play();
     }
