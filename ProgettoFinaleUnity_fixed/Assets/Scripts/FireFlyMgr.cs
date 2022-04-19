c06dc80a3502c7cc2a009c17de304a156308f722
Pacca38 <enrypacca@gmail.com>
2022-04-19 17:25:23 +0200

    Ristrutturazione del target di tutorial

---
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/FireFlyMgr.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireFlyMgr : MonoBehaviour
+{
+    public GameObject[] FireFlies;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
