e5c75b317583227e715ea9f3b6113e815ef14104
ThomasCantoni <thomas.cantoni.2001@gmail.com>
2022-04-19 12:07:49 +0200

    Added volume, added resolutions, added Vsync

--- ProgettoFinaleUnity_fixed/Assets/Scripts/UISaveOptions.cs
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/UISaveOptions.cs
@@ -2,12 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
-
+[System.Serializable]
+public class ResItem
+{
+    public int width, height;
+}
 public class UISaveOptions : MonoBehaviour
 {
     UI_MasterVolumeScript vol;
     public Slider AimSensiSlider, FOVSlider,VolumeSlider;
+    public TMP_Dropdown Dropdown;
+    public Toggle VsyncToggle;
+    public int SelectedResolutionDropdown
+    {
+        get { return Dropdown.value; }
+        set { Dropdown.value = value; }
+    }
     public float AimSens { get; set; }
     public float Fov { get; set; }
     public float Volume
@@ -21,18 +33,53 @@ public class UISaveOptions : MonoBehaviour
             vol.ChangeVolume(value);
         }
     }
-    public int DropdownReso;
+    public int Vsync
+    { 
+        get
+        {
+            if (VsyncToggle.isOn)
+                return 1;
+            else
+                return 0;
+        }
+        set
+        {
+            if (value > 0)
+                VsyncToggle.isOn = true;
+            else
+                VsyncToggle.isOn = false;
+
+        }
+    }
+    [SerializeField]
+    public List<ResItem> Resolutions = new List<ResItem>();
+
     public void OnEnable()
     {
         vol = GetComponent<UI_MasterVolumeScript>();
+        Dropdown.ClearOptions();
+        for (int i = 0; i < Resolutions.Count; i++)
+        {
+            Dropdown.options.Add(new TMP_Dropdown.OptionData(Resolutions[i].width.ToString() + "x" + Resolutions[i].height.ToString()));
+           // Dropdown.options[i].text = Resolutions[i].width.ToString() + "x" + Resolutions[i].height.ToString();
+        }
         AimSensiSlider.value = PlayerPrefs.GetFloat(SaveManager.AimSensitivity);
         FOVSlider.value = PlayerPrefs.GetFloat(SaveManager.FOV);
         VolumeSlider.value = PlayerPrefs.GetFloat(SaveManager.Volume);
+        Vsync = PlayerPrefs.GetInt("VsyncCount");
+        SelectedResolutionDropdown = PlayerPrefs.GetInt("Resolution");
+
     }
     public void SaveOptions()
     {
-        SaveManager.SaveOptions(AimSens, Fov,Volume);
-
+        SaveManager.SaveOptions(AimSens, Fov,Volume,Resolutions[SelectedResolutionDropdown],SelectedResolutionDropdown,Vsync);
+        ApplyGraphics();
+    }
+    void ApplyGraphics()
+    {
+        Screen.fullScreen = true;
+        QualitySettings.vSyncCount = Vsync;
+        Screen.SetResolution(Resolutions[SelectedResolutionDropdown].width, Resolutions[SelectedResolutionDropdown].height, true);
     }
     public void Back()
     {
