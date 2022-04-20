a08b1f438f8ee35af0796e937a3cef5d59403b57
Deomoreo <deomoreo@gmail.com>
2022-04-20 11:47:43 +0200

    tipregodioX2

--- ProgettoFinaleUnity_fixed/Assets/Scripts/BossDoorScript.cs
+++ ProgettoFinaleUnity_fixed/Assets/Scripts/BossDoorScript.cs
@@ -10,6 +10,8 @@ public class BossDoorScript : MonoBehaviour
     public int ActivatedKeys;
     public List<int> KeysID;
     public float StopAudioTimer = 3f;
+    public CameraShakeScript Shake;
+
     public void ActivateKey(int ID)
     {
         if(!KeysID.Contains(ID))
@@ -20,11 +22,13 @@ public class BossDoorScript : MonoBehaviour
         }
         if (ActivatedKeys >= 2)
         {
+            float amount = 3f;
             //this.GetComponent<Animator>().SetTrigger("Open");
             LeftRock.GetComponent<Animator>().SetTrigger("Open");
             RightRock.GetComponent<Animator>().SetTrigger("Open");
-
             CenterRock.GetComponent<Animator>().SetTrigger("Open");
+            Shake.ApplyShake(amount * 1.2f, amount * 2f, amount * 2f);
+
 
             AS.Play();
             Destroy(AS, StopAudioTimer);
